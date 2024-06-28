2cab870ca7deb42ab726fddde6af51182cdc8fd4
prayagraj namdev <prayagraj44@gmail.com>
2024-06-28 23:54:52 +0530

    Initial commit

---
+++ src/test/java/com/checkwithprayag/AzureBlobDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkwithprayag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBlobDataPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
